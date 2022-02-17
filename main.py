cc6be298cf583161df0e5e832988d025a5d72153
vinspdb <57073656+vinspdb@users.noreply.github.com>
2022-02-17 11:19:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from pm import Algo, Order, Miner
 
 log_name = 'road'
 Miner.generate_csv(log_name)
-miner = Miner(log_name, Order.FRQ, Algo.IND, cut=100, top=5, filtering=False, frequency=False, update=True)
-miner.process_stream()
-miner.save_results()
-miner.save_variant_histogram()
+for order, algo, cut, top, filtering, frequency, update in product([Order.FRQ], Algo, [15000], [None], [True, False], [True, False], [True, False]):
+    print(f'{order.name},{algo.name},{cut},{top},{filtering},{frequency},{update}')
+    miner = Miner(log_name, order, algo, cut, top, filtering, frequency, update)
+    try:
+        miner.process_stream()
+        miner.save_results()
+        miner.generate_summary(log_name)
+    except Exception as e:
+        print(f'\tError: {e}')
